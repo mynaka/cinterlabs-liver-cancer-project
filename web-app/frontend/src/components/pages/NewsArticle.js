c4ea96d30e89c8855820b61c04ebceb04cf0dd12
ihvergara <71376611+ihvergara@users.noreply.github.com>
2023-08-11 12:05:10 +0800

    UPDATED NEWS
    Will still fix the news page but the main article is finished

--- web-app/frontend/src/components/pages/NewsArticle.js
+++ web-app/frontend/src/components/pages/NewsArticle.js
@@ -1,36 +1,37 @@
-import React from 'react'
+import React from 'react';
 import '../../App.css';
+import './styles/NewsArticle.css'; // Import the NewsArticle.css file
 import Footer from '../Footer';
-import './styles/NewsCards.css'
 import { useLocation } from 'react-router-dom';
 import Navbar from '../Navbar';
 
 export const NewsArticle = () => {
-
     const location = useLocation();
     const route = "/";
-  return (
-    <div className='body-dark'>
-        <Navbar />
-        <div className='headSpace'></div>
-        <div className='newsArticle'>
-            <div className='articleImg'>
-                <img src={route.concat(location.state.props.photo)} alt='news-img' className='articleImg' ></img>
-            </div>
-            <div className ='articleTitle'>
-                <h1>{location.state.props.title}</h1>
-            </div>
 
-            <div className='articleContent'>
-                <p>{location.state.props.content}</p>
-            </div>
-            
-            <div className='articleDate'>
-                <h6>{Date(location.state.props.date)}</h6>
-            </div>
+    return (
+        <div className='body-dark'>
+            <Navbar />
+            <div className='headSpace'></div>
+            <div className='newsArticle'>
+            <div className='articleTitle'>
+                    <h4>{location.state.props.title}</h4>
+                </div>
+                <div className='articleDate'>
+                    <h6>{Date(location.state.props.date)}</h6>
+                </div>
+                <div className='articleImg'>
+                    <img src={route.concat(location.state.props.photo)} alt='news-img' className='articleImg' />
+                </div>
+               
 
+                <div className='articleContent'>
+                    <p>{location.state.props.content}</p>
+                </div>
+
+                
+            </div>
+            <Footer />
         </div>
-        <Footer/>
-    </div>
-  )
-}
+    );
+};
