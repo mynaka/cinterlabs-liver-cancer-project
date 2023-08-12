5b8733855010c7e0b6fdb33fd9b824ab4f5ecd1f
ihvergara <71376611+ihvergara@users.noreply.github.com>
2023-08-12 22:25:28 +0800

    updated the news home
    will only show the recent news at the home page

--- web-app/frontend/src/components/pages/Home.js
+++ web-app/frontend/src/components/pages/Home.js
@@ -1,25 +1,49 @@
-import React from 'react';
 import '../../App.css';
-import Cards from '../Cards';
+import React, { useEffect, useState } from 'react';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
 import './styles/Home.css'; // Import the Home.css file
 import Navbar from '../Navbar';
+import NewsCards from './NewsCards';
+
 function Home() {
+  const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/news")
+      .then(res => { return res.json() })
+      .then(resData => {
+        console.log(resData);
+        return resData;
+      })
+      .then(news => { setNewsData(news) })
+      .catch(err => console.log(err));
+  }, []);
+
+  const firstNewsCard = newsData.length > 0 ? newsData[0] : null;
+
   return (
     <div>
-      <Navbar/>
-       <div className='home-container'>
-       <div className='spaced'></div>
-      <HeroSection />
-      <Cards />
+      <Navbar />
+      <div className='home-container'>
+        <div className='spaced'></div>
+        <HeroSection />
+        <h1 className='recentUpdates'>Recent Updates</h1>
+        {firstNewsCard && (
+          <NewsCards
+            useHomeStyles={true}
+            id={firstNewsCard._id}
+            photo={firstNewsCard.photo}
+            title={firstNewsCard.title}
+            tags={firstNewsCard.tags}
+            author={firstNewsCard.author}
+            content={firstNewsCard.content}
+            date={firstNewsCard.date_add}
+          />
+        )}
+      </div>
       <Footer />
     </div>
-    </div>
-    
-     
-
-    
   );
 }
 
--- web-app/frontend/src/components/pages/NewsCards.js
+++ web-app/frontend/src/components/pages/NewsCards.js
@@ -1,32 +1,40 @@
-import React from 'react'
-import './styles/NewsCards.css'
-import {useNavigate} from 'react-router-dom';
+import React from 'react';
+import './styles/NewsCards.css';
+import './styles/NewsCardsHome.css';
+import { useNavigate } from 'react-router-dom';
 
-export default function NewsCards(props){
+export default function NewsCards(props) {
     const navigate = useNavigate();
 
     const redirect = () => {
-      navigate('/news/article',{state:{props:props}})  
-    }
-    return(
-        <div className='newsList' onClick={()=>redirect()}>
-            <div key ={props.index} className='newsCard'>
-                <img src = {props.photo} alt='news-img' className='newsImg'></img>
+        navigate('/news/article', { state: { props: props } });
+    };
 
-                <div className='newsCardContent'>
-                    <h3 className='newsTitle'>{props.title}</h3>
-                    <div className='stack'>
-                        <div className='newsAuthor'>{props.author}</div>
+    const useHomeStyles = props.useHomeStyles || false;
+
+    return (
+        <div className={`newsList ${useHomeStyles ? 'newsListHome' : ''}`} onClick={() => redirect()}>
+            <div key={props.index} className={`newsCard ${useHomeStyles ? 'newsCardsHome' : ''}`}>
+                <img src={props.photo} alt='news-img' className={`newsImg ${useHomeStyles ? 'newsImgHome' : ''}`} />
+
+                <div className={`newsCardContent ${useHomeStyles ? 'newsCardContentHome' : ''}`}>
+                    <h3 className={`newsTitle ${useHomeStyles ? 'newsTitleHome' : ''}`}>{props.title}</h3>
+                    <div className={`stack ${useHomeStyles ? 'stackHome' : ''}`}>
+                        <div className={`newsAuthor ${useHomeStyles ? 'newsAuthorHome' : ''}`}>{props.author}</div>
                     </div>
-                    <div className='tagsStack'>
-                        <div className='newsTags'>{props.tags.map((tag,ind) =>(
-                            <div key={ind} className='tagItem'>
-                                <ul className='tag'><li className='tags'>{tag}</li></ul>
-                            </div>
-                        ))}</div>
+                    <div className={`tagsStack ${useHomeStyles ? 'tagsStackHome' : ''}`}>
+                        <div className={`newsTags ${useHomeStyles ? 'newsTagsHome' : ''}`}>
+                            {props.tags.map((tag, ind) => (
+                                <div key={ind} className={`tagItem ${useHomeStyles ? 'tagItemHome' : ''}`}>
+                                    <ul className={`tag ${useHomeStyles ? 'tagHome' : ''}`}>
+                                        <li className={`tags ${useHomeStyles ? 'tagsHome' : ''}`}>{tag}</li>
+                                    </ul>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
